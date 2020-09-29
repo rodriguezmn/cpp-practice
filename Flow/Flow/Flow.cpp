7ee23d6a1332c78515ed08b0e5f9102304351b8a
Marlon Rodriguez <mrbox30@gmail.com>
2020-09-29 17:30:15 -0600

    Add functionality to compare more numbers

--- Flow/Flow/Flow.cpp
+++ Flow/Flow/Flow.cpp
@@ -9,27 +9,43 @@ int main()
 {
     int firstNumber;
     int secondNumber;
+    bool keepGoing = true;
+    int answer;
 
-    // Ask the user for two numbers
-    // Compare the numbers 
-    // Output result 
+    // Compare numbers until the user wants to stop
 
-    cout << "Enter the first number: ";
-    cin >> firstNumber;
-    cout << "You entered: " << firstNumber << '\n';
-    cout << "Enter the second number: ";
-    cin >> secondNumber;
-    cout << "You entered: " << secondNumber << '\n';
-
-    if (firstNumber < secondNumber)
+    while (keepGoing)
     {
-        cout << "The first number is smaller than the second number" << '\n';
+
+        // Ask the user for two numbers
+        // Compare the numbers 
+        // Output result 
+
+        cout << "Enter the first number: ";
+        cin >> firstNumber;
+        cout << "You entered: " << firstNumber << '\n';
+        cout << "Enter the second number: ";
+        cin >> secondNumber;
+        cout << "You entered: " << secondNumber << '\n';
+
+
+        if (firstNumber < secondNumber)
+        {
+            cout << "The first number is smaller than the second number" << '\n';
+        }
+        else if (firstNumber > secondNumber)
+        {
+            cout << "The first number is larger than the second number" << '\n';
+        }
+        else
+            cout << "The numbers are equal" << '\n';
+
+        cout << '\n' << "Do you want to compare another pair? : " << "0 means no, any other number means yes: ";
+        cin >> answer;
+        if (answer == 0)
+        {
+            keepGoing = false;
+        }
     }
-    else if (firstNumber > secondNumber)
-    {
-        cout << "The first number is larger than the second number" << '\n';
-    }
-    else
-        cout << "The numbers are equal" << '\n';
 }
 
