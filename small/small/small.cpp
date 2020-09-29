6398574064d2daf136bf472ce2ae3cea97a2d287
Marlon Rodriguez <mrbox30@gmail.com>
2020-09-29 13:35:39 -0600

    Add mars distance

--- small/small/small.cpp
+++ small/small/small.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "Mars is " << 63.79 << " million km away." << '\n';
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
